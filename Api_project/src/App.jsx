20be9dd8cd4664c1c9a18d693c73af3b431c47c6
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-06-05 15:15:12 +0545

    APi project

---
+++ Api_project/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Navbar from './Navbar'
+
+function App() {
+  const [cards, setcards] = useState([])
+
+  const fetchData = async () => {
+
+    const a = await fetch('https://jsonplaceholder.typicode.com/posts')
+    const data = await a.json()
+    setcards(data)
+   
+  }
+
+  useEffect(() => {
+
+    fetchData()
+
+  }, [])
+
+
+  return (
+    <><Navbar />
+      <div className="container">
+        {cards.map((card) => {
+          return <div key={card.UserId} className="card">
+            <h1>{card.title}</h1>
+            <p>{card.body}</p>
+            <span>By : User id {card.id}</span>
+          </div>
+
+        })}
+
+
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
---
+++ Api_project/src/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+    
+        <ul>
+            <li>Home</li>
+            <li>Service</li>
+            <li>Contact</li>
+            <li>About Us</li>
+        </ul>
+      
+
+    </div>
+  )
+}
+
+export default Navbar
