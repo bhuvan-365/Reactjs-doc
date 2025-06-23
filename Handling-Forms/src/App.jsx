a4bf6f7e4c7bbc59bd8c95bc56070688404eddde
Bhuvan Bhattarai <bhuvanbhattarai07@gmail.com>
2025-06-23 17:34:18 +0545

    handling Form

--- Handling-Forms/src/App.jsx
+++ Handling-Forms/src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import { useForm } from 'react-hook-form'
 import { useState } from 'react'
+import Loader from './loder.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const {
@@ -32,32 +34,33 @@ function App() {
 
 
 
-    return (
-      <>
-        {isSubmitting && <div className="loading">Submitting...</div>}
-        <div className="container">
-          <form action="" onSubmit={handleSubmit(onSubmit)}>
-
-            <input {...register("username", { required: { value: true, message: "username is required" }, minLength: { value: 4, message: "Min length is 4" }, maxLength: { value: 10, message: "Max length is 10" } })} type="text" id="" />
-            <br />
-            {/* for simple error message display
+  return (
+    <>
+      <div className="container">
+        <h1 className='underline'>React-hook-form</h1>
+      {isSubmitting && <div className="loading"><Loader /> submitting...</div>}
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <div className="name">Username :</div>
+          <input {...register("username", { required: { value: true, message: "username is required" }, minLength: { value: 4, message: "Min length is 4" }, maxLength: { value: 10, message: "Max length is 10" } })} type="text" id="box" />
+          <br />
+          {/* for simple error message display
           errors.username && <div className="red">There some error</div> */}
-            {/* for simple error message display
+          {/* for simple error message display
           errors.username && <div className="red">There some error</div> */}
 
-            {errors.username && <div className="red">{errors.username.message}</div>}
+          {errors.username && <div className="red">{errors.username.message}</div>}
+          <div className="pass">Password :</div>
+          <input className='px-2.5' {...register("password", { required: { value: true, message: "password required" }, minLength: { value: 4, message: "Min length is 4" }, maxLength: { value: 10, message: "Max length is 10" } })} type="password" id="box" />
+          <br />
+          {errors.password && <div className="red">{errors.password.message}</div>}
 
-            <input {...register("password", { required: { value: true, message: "password required" }, minLength: { value: 4, message: "Min length is 4" }, maxLength: { value: 10, message: "Max length is 10" } })} type="password" id="" />
-            <br />
-            {errors.password && <div className="red">{errors.password.message}</div>}
+          <input disabled={isSubmitting} type="submit" value="Submit" className='subBtn btn btn-primary p-1 px-2 rounded-4 text-center mb-2 ' />
+          {errors.myform && <div className="red">{errors.myform.message}</div>}
+        </form>
+      </div>
 
-            <input disabled={isSubmitting} type="submit" value="Submit" />
-            {errors.myform && <div className="red">{errors.myform.message}</div>}
-          </form>
-        </div>
+    </>
+  )
+}
 
-      </>
-    )
-  }
-
-  export default App
+export default App
